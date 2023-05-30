77413256c48adbc1f9b2a31f9efde4553309eccc
Daniel Paiva <danielpaiva9@gmail.com>
2023-05-29 23:29:58 -0300

    first commit

---
+++ src/fetchItem.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const AWS = require('aws-sdk');
+
+const fetchItem = async (event) => {
+
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+    const {id} = event.pathParameters;
+
+    let item;
+
+    try  {
+
+        const results = await dynamoDB.scan(
+            {
+                TableName: 'ItemTableNew',
+                Key: {id},
+            }
+        ).promise();
+
+        item = results.Item;
+
+    } catch (error) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(item),
+
+        }
+    }
+}
+
+module.exports = {
+    handler: fetchItem,
+}
